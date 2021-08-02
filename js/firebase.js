3e90b47381b4981897169c007f692fdb516104d5
DANISH <69685883+danish1207@users.noreply.github.com>
2021-08-02 14:59:45 +0530

    danish

---
+++ js/firebase.js
@@ -0,0 +1,43 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCnBn7BqdtvjoVqawwKQ7_jPfjNKnsv1e8",
+    authDomain: "newfirebase-c479e.firebaseapp.com",
+    databaseURL: "https://newfirebase-c479e.firebaseio.com",
+    projectId: "newfirebase-c479e",
+    storageBucket: "newfirebase-c479e.appspot.com",
+    messagingSenderId: "845608843636",
+    appId: "1:845608843636:web:050cc7a96a265a1eda5421",
+    measurementId: "G-HHHWNQVCJS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.auth.Auth.Persistence.LOCAL;
+
+// var button=document.getElementById("btn-login");
+// button.addEventListener("click", function()
+// {
+//   alert("Hi");
+// });
+$("#btn-login").click(function () {
+  var email = $("#emailcurr").val();
+  var password = $("#passwordcurr").val();
+
+  if (email != " " && password != " ") {
+    var result = firebase.auth().signInWithEmailAndPassword(email, password);
+    result.catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("errorcode", errorCode);
+      console.log("errormessage", errorMessage);
+      window.alert("Invalid Login", errorMessage);
+    });
+  } else {
+    window.alert("Please Enter Your Details.");
+  }
+});
+$("#btn-logout").click(function () {
+  firebase.auth().signOut();
+});
+$("#btn-logout1").click(function () {
+
+  firebase.auth().signOut();
+});
